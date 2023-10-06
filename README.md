c246d1e21d98282b5c0f1783ce4684957758e1e0
Shaji-Mohammed <mshajisufiyan@gmail.com>
2023-10-06 20:08:38 -0300

    assn2 first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Assignment 2
+
+- This is a basic python script that prints 'Hello World!'.
---
+++ main.py
@@ -0,0 +1,6 @@
+def print_hi():
+    print(f'Hello World!')
+
+
+if __name__ == '__main__':print_hi()
+
